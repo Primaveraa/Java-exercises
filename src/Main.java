5cbc9aa12a03d6b6ae2856e0966f2e363c4e9d76
Wiosna <wiosnam@emea.roche.com>
2022-10-18 16:43:22 +0200

    Exercise 1

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean isHit = false;
+        int licznik = 0;
+        do {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Pick a number");
+            int user_number = scan.nextInt();
+
+            Random number = new Random();
+            int number_drawn = number.nextInt(1);
+            System.out.println(number_drawn);
+
+            if (user_number == number_drawn) {
+                System.out.println("Hit!");
+                isHit = true;
+            } else {
+                System.out.println("Miss!");
+            }
+            licznik = licznik + 1;
+        }
+        while (isHit == false && licznik < 5);
+
+        System.out.println("The end");
+
+    }
+}
+
+
